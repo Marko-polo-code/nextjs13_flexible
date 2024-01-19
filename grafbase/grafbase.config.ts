d4670f23ae611995c1cf7225c7596f0d38f8e46e
Mark Muller <mark.muller.w@gmail.com>
2024-01-19 17:29:43 +0100

    fixed postgres and grafbase connector

--- grafbase/grafbase.config.ts
+++ grafbase/grafbase.config.ts
@@ -1,27 +1,23 @@
-import { graph, config } from '@grafbase/sdk'
+import { graph, connector, config } from '@grafbase/sdk'
+
 
 const g = graph.Standalone()
 
-const User = g.type('User', {
-  name: g.string().length({ min: 2, max:2 }),
-  email: g.string().unique(),
-  avatarUrl: g.url(),
-  description: g.string(),
-  githubUrl: g.url().optional(),
-  linkedIn: g.url().optional(),
-  projects: g.relation(() => Project).list().optional,
+const pg = connector.Postgres('Postgres', {
+  url: g.env('DATABASE_URL'),
 })
 
-const Project = g.type('Project', {
-  title: g.string().length({ min: 3 }),
-  description: g.string(),
-  image: g.url(),
-  liveSiteUrl: g.url(),
-  githubUrl: g.url(),
-  category: g.string().search(),
-  createdBy: g.relation(() => User),
-})
+g.datasource(pg)
 
 export default config({
   graph: g,
+  // Authentication - https://grafbase.com/docs/auth
+  auth: {
+    // OpenID Connect
+    // const oidc = auth.OpenIDConnect({ issuer: g.env('OIDC_ISSUER_URL') })
+    // providers: [oidc],
+    rules: (rules) => {
+      rules.public()
+    },
+  },
 })
